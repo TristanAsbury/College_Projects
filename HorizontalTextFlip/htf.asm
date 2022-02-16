8823e2e660b1d0615f6607ddf7f3518d899004ab
Tristan Asbury <36208442+TristanAsbury@users.noreply.github.com>
2022-02-15 21:15:17 -0500

    Update htf.asm

--- HorizontalTextFlip/htf.asm
+++ HorizontalTextFlip/htf.asm
@@ -1,93 +1,90 @@
 myStack SEGMENT STACK
+
     DW 256 dup (?)
+
 myStack ENDS
 
 myData SEGMENT
+
 myData ENDS
 
 myCode SEGMENT
+    assume ds: myData, cs: myCode
 
     main PROC
-    assume ds: myData, cs: myCode
-    
-    mov ax, 0b800h
-    mov es, ax
+    push ax es di si cx ; add our register values to stack
+        mov ax, 0b800h  ; move the screen memory address to ax
+        mov es, ax      ; move ax (containing screen memory address) to es 
 
-    mov di, 0       ; di contains address of first char
-    mov si, 158     ; si contains address of last char
-    mov cx, 24      ; ch has a counter for the rows
-    push cx         ; for the first iteration, we must push the cx register onto the stack
+        mov di, 0       ; di contains address of first char
+        mov si, 158     ; si contains address of last char
+        mov cx, 25      ; cx has a counter for the rows
 
-    rowLoop:
-        pop cx          ; pop the value of the row counter
-        cmp cx, 0       ; are we on the last row?
-        je done         ; if so, we are done
-        dec cx          ; if not, then dec
-        push cx si di   ; push all of our needed registers on the stack
-        mov cx, 40      ; set cx to the column counter
-        jmp colLoop     ; switch lines
+        rowLoop:
+            call switch
+            loop rowLoop
 
-    colLoop:
-        cmp cx, 0           ; are we on the last character to switch?
-        je nextLine         ; if so, go to next line
- 
-        mov ax, es:[di]     ; store the character at screen offset di in ax 
-        mov bx, es:[si]     ; store the character at screen offset si in bx 
-
-        mov es:[di], bx     ; place the character in bx in screen offset at di
-        mov es:[si], ax     ; place the character in ax in screen offset at si
-
-        add di, 2           ; add 2 to di, moving the left position to the next character
-        sub si, 2           ; sub 2 from si, moving the right position to the previous character
-
-        loop colLoop
-
-    nextLine:
-        pop di si           ; pop values of si and di from the stack
-        add di, 160         ; add 160 to both, going to the next line
-        add si, 160         ; 
-        jmp rowLoop         ; go back to the rowLoop label
-    
-    done:
-        call finish
-
+        call color
+        pop cx si di es ax  ; pop register values from stack
+        mov AH, 4Ch     ; These two instructions use a DOS interrupt
+        int 21h             
+        ret
     main ENDP
 
-    finish PROC
-        mov cx, 80*25*2     ; number of characters on screen
-        mov si, 0
+    switch PROC 
+        push ax bx cx si di     ; add our register values to the stack
+        mov cx, 40              ; set the counter to 40 (how many cols there are each side)
 
         top:
-            cmp cx, 0           ; are we on the last character
-            je exit             ; if so, exit
-            mov ax, es:[si]
+            mov ax, es:[di]     ; store the character at screen offset di in ax 
+            mov bx, es:[si]     ; store the character at screen offset si in bx 
+            mov es:[di], bx     ; place the character in bx in screen offset at di
+            mov es:[si], ax     ; place the character in ax in screen offset at si
+            add di, 2           ; add 2 to di, moving the left position to the next character
+            sub si, 2           ; sub 2 from si, moving the right position to the previous character
+            loop top
 
-            cmp al, 'A'
-            jl dontColor
-            cmp al, 'Z'
-            jle colorIt
+        pop di si cx bx ax      ; pop register values from stack
+        add si, 160             ; set the char pointers to next line
+        add di, 160
+        ret                     ; return
+    switch ENDP
+
+    color PROC
+        push ax bx cx si di     ; add our registers to the stack
+        mov cx, 25*80           ; set the counter to 4000
+        mov si, 0               ; set the char pointer to first position
+
+        topColor:
+            cmp cx, 0           ; are we on the last character?
+            je exit             ; if so, jump to exit
+            mov ax, es:[si]     ; if not, then move the character into ax
+            cmp al, 'A'         ; test to see if its a valid character
+            jl dontColor        ; if not, dont color it
+            cmp al, 'Z'         
+            jle doColor         ; if it is, do color it
             cmp al, 'a'
-            jl dontColor
+            jl dontColor        ; if not, dont
             cmp al, 'z'
-            jle colorIt
+            jle doColor         ; if it is, do
 
-        colorIt:
-            mov ah, 01110001b   ; change the color of the character
-            mov es:[si], ax     ; move the chracter to screen memory
-            dec cx              ; decrement counter
-            add si, 2           
-            jmp top
-
+        doColor:
+            mov ah, 01110001b   ; set the background to white with blue colored text
+            mov es:[si], ax     ; move the character color back into screen memory
+            add si, 2           ; move the char pointer to next char
+            dec cx              ; decrement the counter
+            jmp topColor        ; go back to top
+        
         dontColor:
-            mov ah, 00000111b   ; change the color of the character 
-            mov es:[si], ax     ; move the character to screen memory
-            add si, 2           ; decrement counter
-            dec cx
-            jmp top
+            mov ah, 00000111b   ; set the background to black with grey colored text
+            mov es:[si], ax     ; move the characer color back into screen memory
+            add si, 2           ; move the char pointer to next char
+            dec cx              ; decrement the counter
+            jmp topColor        ; go back to top
 
         exit:
-            mov AH, 4Ch     ; These two instructions use a DOS interrupt
-            int 21h  
-    finish ENDP
+        pop di si cx bx ax      ; pop register values from stack
+        ret                     ; return
+    color ENDP
 myCode ENDS
 END main
