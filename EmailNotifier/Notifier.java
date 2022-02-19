2a4f0d72ef93ac90001885e6b1cd4f7514ba23bd
Tristan Asbury <tasbury2@students.fairmontstate.edu>
2022-02-19 12:18:28 -0500

    Started EmailNotifier

---
+++ EmailNotifier/Notifier.java
@@ -0,0 +1,74 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+
+public class Notifier implements ActionListener {
+    SystemTray tray;
+    PopupMenu trayPopup;
+    Timer timer;
+    int intervalMinutes;
+    Properties props;
+
+    public Notifier(){
+        System.out.println("Creating tray!");
+        if(SystemTray.isSupported()){               //If there is a system tray that is supported.
+            tray = SystemTray.getSystemTray();      //Then we will make a pointer to that tray
+            
+            trayPopup = new PopupMenu();            //Create a new popup menu for the tray item
+
+            MenuItem simpleThing = new MenuItem("Hello!");  //Add the menu items
+            trayPopup.add(simpleThing);                     //Add the menu items to the popup
+
+            setupProps();
+            setupTray();                                    //Setup the tray
+
+        } else {
+            System.out.println("Failed to find system tray!");
+        }
+    }
+
+    private void setupProps(){
+        try {
+            props.load(new FileInputStream("props"));
+        } catch (IOException e){
+            
+            props = new Properties();
+            props.setProperty("username", "bob");
+        }
+    }
+
+    private void setupTray(){
+        System.out.println("Creating tray!");
+        TrayIcon trayIcon = new TrayIcon(Toolkit.getDefaultToolkit().getImage("icon.png"), "My popup example tooltip text.");
+        trayIcon.setImageAutoSize(true);
+        trayIcon.setPopupMenu(trayPopup);
+        
+        try{
+            tray.add(trayIcon);
+        } catch (AWTException e){
+            System.out.println("Failed to add icon!");
+            return;     //If the setup fails, return to the constructor, and pretty much end the program.
+        }
+        System.out.println("We made da tray icon!");
+
+        timer = new Timer(1000, this);
+        timer.setRepeats(true);
+        timer.setActionCommand("CHECK");
+        timer.start();      
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        if(e.getActionCommand().equals("CHECK")){
+            System.out.println("Hello!");
+        }
+    }
+
+    private void exit(){
+
+    }
+}
