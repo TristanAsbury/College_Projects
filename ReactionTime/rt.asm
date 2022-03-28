b67d7b4f6761b80c5ee1e7d15d9eda86b4601c84
Tristan Asbury <36208442+TristanAsbury@users.noreply.github.com>
2022-03-28 11:36:36 -0400

    Update rt.asm

--- ReactionTime/rt.asm
+++ ReactionTime/rt.asm
@@ -1,118 +1,73 @@
-;=====================
+;=========SEGMENT============
 myStack SEGMENT STACK
     DB 256 DUP (?)
 myStack ENDS
-;=====================
+;=========END OF SEGMENT=====
 
-;=====================
+;=========SEGMENT============
 myData SEGMENT
-    randomLetters DB 3 DUP (?)
-    copiedLetters DB 3 DUP (?)
-    seed DW 1
-    targetWaitTime DW (?)
-    numCorrect DB 0
-
-    
+    randomLetters DB 3 DUP (?)  ; original letters
+    copiedLetters DB 3 DUP (?)  ; letters copied over
+    seed DW 1                   ; seed is initally 1
+    targetWaitTime DW (?)       ; target wait time
+    numCorrect DB 0             ; number of correct letters typed
+    startTime DW (?)            ; start time used for the timer
 myData ENDS
-;=====================
+;=========END OF SEGMENT=====
 
+;=========SEGMENT============
 myCode SEGMENT
     assume ds: myData, cs: myCode
 
-;=====================
+;============================
 main PROC
     mov ax, 0b800h
     mov es, ax
     mov ax, myData
     mov ds, ax
 
-    ; clear screen
-    call clearScreen
-    call pickRandomChars
-    call copyChars
-    call waitTime
-    call scatterChars
+    call clearScreen        ; clear the screen
+    call pickRandomChars    ; then pick the random characters
+    call copyChars          ; then copy the characters into the copiedLetters
+    call waitTime           ; then wait a random amount of time
+    call scatterChars       ; then scatter the chosen characters across the screen 
+    call startTimer         ; start the timer
 
 topMainLoop:
-    call showCorrect
-    call showLetterSets
-    mov ah, 11h
+    call updateTimer        ; update the timer
+    mov ah, 11h             ; look in the key buffer
     int 16h
-    jz topMainLoop
-    mov ah, 10h
-    int 16h ; get key from buffer
-    cmp al, 27
-    je exitMain
-    call handleChar
-    jmp topMainLoop
+    jz topMainLoop          ; if its empty, then jump to top
+    mov ah, 10h             ; else, get the key
+    int 16h                 ; ^^
+    cmp al, 27              ; is it the esc key?
+    je exitMain             ; if so, exit
+    call handleChar         ; else, handle the character they typed
+    jmp topMainLoop         ; go back to top
 
 exitMain:
-    call endGame
+    call endGame            ; end the game
 
 main ENDP
-;=====================
+;============================
 
-;=====================
-showCorrect PROC
-    push ax
-    mov al, numCorrect
-    add al, '0'
-    mov es:[160], al
-    pop ax
-    ret
-showCorrect ENDP
-;=====================
-
-;=====================
-showLetterSets PROC
-    push ax cx si di
-
-    lea si, randomLetters
-    mov di, 320
-    mov cx, 0
-topShowOriginal:
-    mov al, ds:[si]
-    mov es:[di], al         ; mov char from randomLetters onto screen space di
-    inc cx                  ; next char offset
-    inc si
-    add di, 2               ; next screen pos
-    cmp cx, 3               ; are we at last char
-    jne topShowOriginal     ; if not, then go to next char
-
-    lea si, copiedLetters   ; if so, go to copied letters
-    mov di, 480
-    mov cx, 0
-topShowCopied:
-    mov al, ds:[si]
-    mov es:[di], al         ; mov char from randomLetters onto screen space di
-    inc cx                  ; next char offset
-    inc si
-    add di, 2               ; next screen pos
-    cmp cx, 3               ; are we at last char
-    jne topShowCopied     ; if not, then go to next char
-
-    pop di si cx ax
-    ret
-showLetterSets ENDP
-;=====================
-
-;=====================
+;============================
 endGame PROC
     mov ah, 4ch
     int 21h
 endGame ENDP
-;=====================
+;============================
 
-;=====================
+;============================
 copyChars PROC
     push ax cx si di
-    lea si, randomLetters
-    lea di, copiedLetters
-    mov cx, 0
+    lea si, randomLetters   ; move address of randomLetters into si
+    lea di, copiedLetters   ; move address of the copiedLetters into di
+    mov cx, 0               ; cx := 0
 
 topCopyLoop:
-    cmp cx, 3
-    je exitCopy
+    cmp cx, 3       ; is cx == 3
+    je exitCopy     ; if so, exit
     mov al, ds:[si] ; mov char into al
     mov ds:[di], al ; mov char in al into ds:[di] (the copiedLetters)
     inc si          ; go to next char in randomLetters
@@ -124,9 +79,9 @@ exitCopy:
     pop di si cx ax
     ret
 copyChars ENDP
-;=====================
+;============================
 
-;=====================
+;============================
 clearScreen PROC
     push si
     mov si, 0
@@ -143,9 +98,9 @@ exitClear:
     pop si
     ret
 clearScreen ENDP
-;=====================
+;============================
 
-;=====================
+;============================
 ;on entry:
 ;       look at the CARRY flag, if its true, then that means the player typed a correct char
 
@@ -157,19 +112,18 @@ handleGameState PROC
     ret
 
 foundCorrectChar:
-    inc numCorrect
-    cmp numCorrect, 3
-    je playerWin
-    ret
+    inc numCorrect      ; increment numCorrect
+    cmp numCorrect, 3   ; is there 3 correct?
+    je playerWin        ; if so, the player wins
+    ret                 ; if not, return
 
-playerWin:
-    call endGame
+playerWin:              
+    call endGame        ; end the game
     ret
-
 handleGameState ENDP
-;=====================
+;============================
 
-;=====================
+;============================
 handleChar PROC
 ; on entry:
 ;   AL: contains the character we are looking for
@@ -202,16 +156,10 @@ charNotFound:
     call handleGameState
     ret
 handleChar ENDP
-;=====================
+;============================
 
-;=====================
+;============================
 pickRandomChars PROC
-    ; use the random number generator
-    ; compare that character to the characters currently in randomLetters (using a loop)
-    ; if that character is already there, then find another random number, keep doing this
-
-    ; once we have a new character, then display it somewhere random on the screen
-    ; do this by using the random number generator, 
     push ax bx si di
     mov bx, 0   ; this keeps count of how many successful characters have been pushed into 'randomLetters'
     lea si, randomLetters   ; pointing to the first character at 'randomLetters'
@@ -237,9 +185,9 @@ exitPickRandomChar:
     pop di si bx ax
     ret
 pickRandomChars ENDP
-;=====================
+;============================
 
-;=====================
+;============================
 letterInList PROC
 ; on entry:
 ;   AL: contains the character we are looking for
@@ -260,80 +208,66 @@ notFound:
     clc
     ret
 letterInList ENDP
-;=====================
+;============================
 
-;=====================
+;============================
 getRandomNumber PROC
     push bp
     mov bp, sp
-    push bx
+    push bx cx dx
 
     mov ah, 00h ; stores low order of ticks in DX
     int 1ah
-
     mov ax, dx
     mov bx, seed    ; move the previous seed into bx
     mul bx          
     add ax, 1123    ; add large prime number
-
     ; the process above is equal to: a*r0+b where a and b are large prime numbers
     
-    mov dx, 0       ; clear dx of any thing to allow for a divide
-
+    mov dx, 0       ; clear dx of any thing to allow for a divide without overflow errors
     mov bx, [bp+4]  ; move the high bound into bl
     div bx          ; divide ax by the high bound
-
-    mov ax, dx  ; put remainder into ax
+    mov ax, dx      ; put remainder into ax
     mov seed, ax    ; make the seed the last result
 
-    pop bx bp
+    pop dx cx bx bp
     ret
 getRandomNumber ENDP
-;=====================
+;============================
 
-;=====================
+;============================
 scatterChars PROC
-    push ax di
-    ; MAKE THIS INTO A LOOP
+    push ax cx di
+    mov cl, 0
+    lea si, copiedLetters
+topScatter:
+    cmp cl, 3
+    je exitScatter
     push word ptr 1920
     call getRandomNumber
     add sp, 2
     shl ax, byte ptr 1
     mov di, ax
-    mov al, copiedLetters
+    mov al, ds:[si]
     mov es:[di], al
-    
-    push word ptr 1920
-    call getRandomNumber
-    add sp, 2
-    shl ax, byte ptr 1
-    mov di, ax
-    mov al, copiedLetters+1
-    mov es:[di], al
-    
-    push word ptr 1920
-    call getRandomNumber
-    add sp, 2
-    shl ax, byte ptr 1
-    mov di, ax
-    mov al, copiedLetters+2
-    mov es:[di], al
+    inc cx
+    inc si
+    jmp topScatter
 
-    pop di ax
+exitScatter:
+    pop di cx ax
     ret
 scatterChars ENDP
-;=====================
+;============================
 
-;=====================
+;============================
 waitTime PROC
     push ax bx cx dx
 
     mov ah, 00h  
     int 1ah         ; gets ticks CX:DX
                     ; dx has low order
-
     mov targetWaitTime, dx  ; make that our BASE target time
-
     push word ptr 30
     call getRandomNumber    ; ax will contain 0 - 30
     add sp, 2
@@ -352,9 +286,105 @@ exitWaitLoop:
     pop dx cx bx ax
     ret
 waitTime ENDP
-;=====================
+;============================
 
+;============================
+startTimer PROC
+    push cx ax dx
+    mov ah, 00h
+    int 1ah             ; get current ticks into dx
+    mov startTime, dx   ; move dx into startTime
+    pop dx ax cx
+    ret
+startTimer ENDP
+;============================
+
+;============================
+updateTimer PROC
+    push ax
+
+    push startTime          ; else, push the startTime
+    call getTimeLapseTenths ; returns, in ax, the tenths of seconds since start time
+    push ax                 ; push those tenths of seconds onto stack as an argument for showing time
+    call showTime           ; call show time
+
+    pop ax
+    ret
+updateTimer ENDP
+;============================
+
+;============================
+getTimeLapseTenths PROC
+    push bp         ; push bp on stack to preserve
+    mov bp, sp      ; make bp point to sp (which is currently pointing to preserved bp)
+    push dx bx      ; preserve bx and dx
+
+    mov ah, 00h     ; call the interrupt to get current ticks
+    int 1ah
+
+    sub dx, [bp+4]  ; subtract startTime from current time to get elapsed time of game
+    mov ax, dx      ; move that time into ax
+    mov bx, 55      ; multiply by 55 to get total amount of milliseconds
+    mul bx          
+
+    mov bx, 100     ; divide by 100 to get tenths of the seconds
+    div bx
+    
+    pop bx dx bp
+    ret 2           ; return to move stack pointer back to return address
+getTimeLapseTenths ENDP
+;============================
+
+;============================
+showTime PROC   
+    push bp         ; preserve bp
+    mov bp, sp      ; make bp point to sp (which is pointing to preserved bp)
+    push ax bx dx si; preserve all other registers
+    mov ax, [bp+4]  ; move into ax, the amount of tenths
+    mov bx, 10      ; move 10 into bx
+    mov si, 0       ; make si 0
+
+pushDigitTop:
+    cmp si, 4       ; see if we have reached the last digit
+    je nextStep     ; if so, go to the next step
+
+    mov dx, 0       ; if not, make dx 0
+    div bx          ; divide ax by 10
+    add dx, '0'     ; add '0' to dx to get the character
+    push dx         ; push the character to the stack
+    inc si          ; go to next position
+    jmp pushDigitTop;jmp to top
+
+nextStep:
+    mov dx, 0       ; do final step
+    div bx      
+    add dx, '0'
+    mov es:[160*24], dl ; put the character into that position
+    mov si, 2           ; next position
+
+topShowLoop:
+    cmp si, 12      ; are we at the last printing position
+    je exitShow     ; if so, exit
+    cmp si, 8       ; are we at the position for the decimal
+    je putDecimal   ; if so, do the decimal
+    pop dx          ; else, pop the value at the current position in the timer
+    mov es:[160*24+si], dl  ;print the value on the screen position
+    add si, 2       ; next screen position
+    jmp topShowLoop ; back to top
+
+putDecimal:
+    mov es:[160*24+si], byte ptr '.'
+    add si, 2
+    jmp topShowLoop
+
+exitShow:
+    pop si dx bx ax bp
+    ret 2
+
+showTime ENDP
+;============================
 
 myCode ENDS
+;=========END OF SEGMENT=====
 
 end main
